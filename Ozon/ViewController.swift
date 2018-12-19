ac257978516fe668f63bb8dd1bce0117b8607ac8
Milos Otasevic <otasevic322@gmail.com>
2018-12-19 11:24:58 +0100

    first commit

---
+++ Ozon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ozon
+//
+//  Created by Milos Otasevic on 19/12/2018.
+//  Copyright © 2018 Milos Otasevic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+print("    ")    }
+
+
+}
+
